e6f45dc26fc273848047005230ec39a698d4239b
Herve Pages <hpages@fhcrc.org>
2012-03-21 06:40:28 +0000

    Start pasillaBamSubset package.
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/pasillaBamSubset@2019 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+###
+
+untreated1_chr4 <- function()
+{
+    system.file("extdata", "untreated1_chr4.bam",
+                package="pasillaBamSubset", mustWork=TRUE)
+}
+
+untreated3_chr4 <- function()
+{
+    system.file("extdata", "untreated3_chr4.bam",
+                package="pasillaBamSubset", mustWork=TRUE)
+}
+
